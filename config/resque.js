896d38082c6914e180542a72c691a12305f644d0
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-08 23:01:06 -0400

    Tweak creds.

--- config/resque.js
+++ config/resque.js
@@ -2,7 +2,9 @@ var resque  = require('coffee-resque'),
     app     = require('../server');
 
 module.exports = resque.connect({ 
-  host: app.set('credentials').redis.host
+  host: app.set('credentials').redis.host,
+  port: app.set('credentials').redis.port,
+  pass: app.set('credentials').redis.pass
 });
 
 console.log('Connected to Redis');
