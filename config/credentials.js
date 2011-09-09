3a895b0bd7a755cb8b41b3170f9a0a80e1b00ddf
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-08 22:34:23 -0400

    Bump creds into their own configuration file.

---
+++ config/credentials.js
@@ -0,0 +1,12 @@
+var app = require('../server');
+
+app.set('credentials', {
+  redis: { 
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    pass: process.env.REDIS_PASS
+  }, 
+  mongo: { 
+    url:  process.env.MONGO_URL
+  }
+});
